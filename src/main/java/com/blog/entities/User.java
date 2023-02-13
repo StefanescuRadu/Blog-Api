d928f3fcbc40246528133d6dcbd176667ddc0a2a
Stefanescu Radu <radu98fm@gmail.com>
2023-02-13 17:46:14 +0100

    Added Post entity and set up relations between entities

--- src/main/java/com/blog/entities/User.java
+++ src/main/java/com/blog/entities/User.java
@@ -1,10 +1,15 @@
 package com.blog.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,7 +28,11 @@ public class User {
 	
 	@Column(name = "user_name", nullable = false, length = 100)
 	private String name;
+	
 	private String email;
 	private String password;
 	private String about;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Post> posts = new ArrayList<>();
 }
